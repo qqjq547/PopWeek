54df2065cf8c617c3e4c1b53b46787d876634dfc
qqjq547 <qqjq547@gmail.com>
2014-10-12 18:01:45 +0800

    12222
    65666

--- src/com/hjq/app/MyApplication.java
+++ src/com/hjq/app/MyApplication.java
@@ -1,4 +1,4 @@
-package com.hjq.app;
+﻿package com.hjq.app;
 
 import android.app.Application;
 import android.content.Context;
@@ -17,6 +17,8 @@ public class MyApplication extends Application{
 		    super.onCreate();
 			mInstance = this;
 			initEngineManager(this);
+///////
+
 		}
 		
 		public void initEngineManager(Context context) {
